512fbcdded3bd047cdea9e5e2e19ca1473c55a43
great <great__jin@163.com>
2022-06-23 14:30:21 +0800

    feat: Spring Boot集成ES

--- demo-elasticsearch/es-spring/src/main/java/com/ibudai/config/ESConfig.java
+++ demo-elasticsearch/es-spring/src/main/java/com/ibudai/config/ESConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ElasticsearchConfiguration {
+public class ESConfig {
 
     @Value("${elasticsearch.host}")
     private String host;
