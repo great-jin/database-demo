f96b2a370734bd10270a117263b46469499cd33b
great <great__jin@163.com>
2022-07-04 18:00:17 +0800

    feat: mysql、oracle demo chron: pom依赖调整

---
+++ demo-oracle/oracle-spring/src/main/java/xyz/ibudai/OracleSpringApplication.java
@@ -0,0 +1,13 @@
+package xyz.ibudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleSpringApplication.class, args);
+    }
+
+}
