1991420b66132d9ad079d7fe895f4a3b4e588335
great <great__jin@163.com>
2022-07-05 14:46:28 +0800

    fix: mysql 配置文件修改

--- demo-mysql/mysql-spring/src/main/java/xyz/ibudai/MysqlSpringApplication.java
+++ demo-mysql/mysql-spring/src/main/java/xyz/ibudai/MysqlSpringApplication.java
@@ -1,8 +1,10 @@
 package xyz.ibudai;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("xyz.ibudai.dao")
 @SpringBootApplication
 public class MysqlSpringApplication {
 
