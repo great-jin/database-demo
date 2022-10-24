b7c05ebce1465bc3e3fb8c6d2344badfbe6f011b
great <great__jin@163.com>
2022-10-24 15:14:38 +0800

    feat: 新增 Oracle 表字段信息获取

--- demo-oracle/oracle-maven/src/main/java/xyz/ibudai/InfoTest.java
+++ demo-oracle/oracle-maven/src/main/java/xyz/ibudai/InfoTest.java
@@ -9,6 +9,7 @@ import java.sql.*;
 import java.util.*;
 
 public class InfoTest {
+
     final String CLASSNAME = "oracle.jdbc.OracleDriver";
     final String JDBC = "jdbc:oracle:thin:@//10.231.6.65:1521/helowin";
     final String USERNAME = "budai";
@@ -30,48 +31,23 @@ public class InfoTest {
     public void TableDemo() {
         List<Map<String, String>> tableDTO = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection(JDBC, USERNAME, PASSWORD)) {
-            try {
-                DatabaseMetaData databaseMetadata = connection.getMetaData();
-                String schema = "budai";
-                String[] queryType = new String[]{"TABLE", "VIEW"};
-                ResultSet rs = databaseMetadata.getTables(null, schema, null, queryType);
+            String schema = "budai";
+            String[] queryType = new String[]{"TABLE", "VIEW"};
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet rs = metaData.getTables(null, schema, null, queryType);
 
-                while (rs.next()) {
-                    Map<String, String> tableVo = new HashMap<>();
-                    String tableSchema = StrUtil.isBlank(rs.getString("TABLE_SCHEM")) ? schema : rs.getString("TABLE_SCHEM");
-                    tableVo.put("TableSchema", tableSchema);
-                    tableVo.put("TableName", rs.getString("TABLE_NAME"));
-                    tableVo.put("TableType", rs.getString("TABLE_TYPE"));
-                    //                    tableVo.put("TableCatalog", rs.getString("TABLE_CAT"));
-                    //                    tableVo.put("Remarks", rs.getString("REMARKS"));
-                    tableDTO.add(tableVo);
-                }
-            } catch (Exception e) {
-                tableDTO.clear();
-                PreparedStatement prepareStatement = connection.prepareStatement(
-                        "SELECT NULL AS table_cat,\n" +
-                                "                o.owner AS table_schem,\n" +
-                                "                        o.object_name AS table_name,\n" +
-                                "                o.object_type AS table_type,\n" +
-                                "                        NULL AS remarks\n" +
-                                "                FROM all_objects o\n" +
-                                "                WHERE o.owner LIKE ? ESCAPE '/'\n" +
-                                "                AND o.object_type IN ('xxx', 'TABLE', 'VIEW')\n" +
-                                "                ORDER BY table_type, table_schem, table_name"
-                );
-                prepareStatement.setString(1, "budai");
-                ResultSet rs = prepareStatement.executeQuery();
-                while (rs.next()) {
-                    Map<String, String> tableVo = new HashMap<>();
-                    String tableSchema = StrUtil.isBlank(rs.getString("TABLE_SCHEM")) ? "schema" : rs.getString("TABLE_SCHEM");
-                    tableVo.put("TableSchema", tableSchema);
-                    tableVo.put("TableName", rs.getString("TABLE_NAME"));
-                    tableVo.put("TableType", rs.getString("TABLE_TYPE"));
-                    tableDTO.add(tableVo);
-                }
+            while (rs.next()) {
+                Map<String, String> tableVo = new HashMap<>();
+                String tableSchema = StrUtil.isBlank(rs.getString("TABLE_SCHEM")) ? schema : rs.getString("TABLE_SCHEM");
+                tableVo.put("TableSchema", tableSchema);
+                tableVo.put("TableName", rs.getString("TABLE_NAME"));
+                tableVo.put("TableType", rs.getString("TABLE_TYPE"));
+                tableVo.put("TableCatalog", rs.getString("TABLE_CAT"));
+                tableVo.put("Remarks", rs.getString("REMARKS"));
+                tableDTO.add(tableVo);
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         System.out.println(tableDTO);
     }
@@ -84,8 +60,8 @@ public class InfoTest {
         List<String> schema = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection(JDBC, USERNAME, PASSWORD)) {
             try {
-                DatabaseMetaData databaseMetadata = connection.getMetaData();
-                ResultSet rs = databaseMetadata.getSchemas();
+                DatabaseMetaData metaData = connection.getMetaData();
+                ResultSet rs = metaData.getSchemas();
                 while (rs.next()) {
                     schema.add(rs.getString(1));
                 }
@@ -99,27 +75,54 @@ public class InfoTest {
                     schema.add(rs.getString(1));
                 }
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         System.out.println(schema);
     }
 
-    /**
-     * 获取所有 Schema
-     */
     @Test
-    public void Schema1Demo() {
-        List<String> schema = new ArrayList<>();
+    public void columnsInfo() throws SQLException {
+        String schema = "BUDAI";
+        String tableName = "TB_22091901";
+        List<Map<String, String>> tableMap = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection(JDBC, USERNAME, PASSWORD)) {
-            PreparedStatement statement = connection.prepareStatement("select username as table_schem from all_users order by table_schem");
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                schema.add(rs.getString(1));
+            try {
+                // 获取主键列表
+                List<String> primaryKeyList = new ArrayList<>();
+                DatabaseMetaData metaData = connection.getMetaData();
+                ResultSet primaryKeyRs = metaData.getPrimaryKeys(null, schema, tableName);
+                while (primaryKeyRs.next()) {
+                    String name = primaryKeyRs.getString("COLUMN_NAME");
+                    if (!primaryKeyList.contains(name)) {
+                        primaryKeyList.add(name);
+                    }
+                }
+
+                ResultSet columnRs = metaData.getColumns(null, schema, tableName, null);
+                while (columnRs.next()) {
+                    Map<String, String> columnVo = new LinkedHashMap<>();
+                    String columnName = columnRs.getString("COLUMN_NAME");
+                    Integer isPrimaryKey = primaryKeyList.contains(columnName) ? 1 : 0;
+                    columnVo.put("ColumnName", columnName);
+                    columnVo.put("IsPrimaryKey", isPrimaryKey == 1 ? "yes" : "no");
+                    columnVo.put("DataType", columnRs.getString("DATA_TYPE"));
+                    columnVo.put("TypeName", columnRs.getString("TYPE_NAME"));
+                    columnVo.put("ColumnSize", columnRs.getString("COLUMN_SIZE"));
+                    columnVo.put("DecimalDigits", columnRs.getString("DECIMAL_DIGITS"));
+                    columnVo.put("Nullable", columnRs.getString("NULLABLE"));
+                    columnVo.put("IsNullable", columnRs.getString("IS_NULLABLE"));
+                    columnVo.put("Remarks", columnRs.getString("REMARKS"));
+                    columnVo.put("ColumnDef", columnRs.getString("COLUMN_DEF"));
+                    columnVo.put("NumPrecRadix", columnRs.getString("NUM_PREC_RADIX"));
+                    columnVo.put("CharOctetLength", columnRs.getString("CHAR_OCTET_LENGTH"));
+                    columnVo.put("OrdinalPosition", String.valueOf(columnRs.getInt("ORDINAL_POSITION")));
+                    tableMap.add(columnVo);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
-        System.out.println(schema);
+        System.out.println(tableMap);
     }
 }
