91d13eaba94a2344edbf00385067bf6496bbd3b3
great <great__jin@163.com>
2022-12-09 09:19:48 +0800

    fix：完善 Hive 模块

--- demo-hive/hive-maven/src/main/java/xyz/ibudai/config/HiveConfig.java
+++ demo-hive/hive-maven/src/main/java/xyz/ibudai/config/HiveConfig.java
@@ -5,14 +5,13 @@ import java.sql.DriverManager;
 
 public class HiveConfig {
 
-    private static String url = "jdbc:hive2://192.168.173.43:10000/a_db";
-    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
-
+    private final static String URL = "jdbc:hive2://192.168.173.43:10000/a_db";
+    private final static String DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver";
 
     public static Connection getHiveConnection() {
         try {
-            Class.forName(driverName);
-            return DriverManager.getConnection(url);
+            Class.forName(DRIVER_NAME);
+            return DriverManager.getConnection(URL);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
