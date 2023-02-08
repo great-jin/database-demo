7a063550a9ac91a9b6df6d744c24c1c6173c14f9
great <great__jin@163.com>
2023-02-08 16:02:05 +0800

    fix: 完善 BasicDataSource 连接池配置

--- demo-pool/src/main/java/xyz/ibudai/utils/BasicPoolUtil.java
+++ demo-pool/src/main/java/xyz/ibudai/utils/BasicPoolUtil.java
@@ -1,7 +1,5 @@
 package xyz.ibudai.utils;
 
-import java.util.Properties;
-
 import org.apache.commons.dbcp.BasicDataSource;
 import xyz.ibudai.model.JDBCProperty;
 
@@ -17,18 +15,13 @@ public class BasicPoolUtil {
         dataSource.setUsername(property.getUser());
         dataSource.setPassword(property.getPassword());
         // 初始化连接池大小
-        dataSource.setInitialSize(10);
-        // 连接池的最大数据库连接数, 为 0 表示无限制
-        dataSource.setMaxActive(10);
-        // 最大建立连接等待时间, 如果超过此时间将接到异常。设为 -1 表示无限制
+        dataSource.setInitialSize(3);
+        // 最大数据库连接数, 为 0 表示无限制
+        dataSource.setMaxActive(6);
+        // 最大空闲连接数, 超过即被释放, 为 0 表示无限制
+        dataSource.setMaxIdle(3);
+        // 建立连接最大等待时间, 为 -1 表示无限制
         dataSource.setMaxWait(3000);
-        // 数据库连接的最大空闲时间, 超过空闲时间数据库连接将被标记为不可用，然后被释放, 为 0 表示无限制。
-        dataSource.setMaxIdle(1000);
-
-        Properties connectProps = new Properties();
-        // 空字符串问题
-        connectProps.put("useFetchSizeWithLongColumn", "true");
-        dataSource.setConnectionProperties(connectProps.toString());
         return dataSource;
     }
 }
