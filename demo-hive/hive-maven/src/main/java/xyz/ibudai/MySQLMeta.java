1156aa8dcfa4ddd7383bbbd52f9679c99ea649d1
great <great__jin@163.com>
2023-02-09 16:09:05 +0800

    fix: 完善hive元数据获取

--- demo-hive/hive-maven/src/main/java/xyz/ibudai/MySQLMeta.java
+++ demo-hive/hive-maven/src/main/java/xyz/ibudai/MySQLMeta.java
@@ -6,6 +6,8 @@ import xyz.ibudai.util.MetaUtils;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -32,14 +34,46 @@ public class MySQLMeta {
     @Test
     public void demo2() {
         try (Connection conn = BasicPoolUtil.buildDatasource().getConnection()) {
-            List<Map<String, String>> meta1 = MetaUtils.getNonePartitionTableNum(conn, schema, table);
-            System.out.println("【" + table + "】 num: " + meta1);
+            Map<String, Integer> tableNum1 = MetaUtils.getNonePartitionTableNum(conn, schema, table);
+            System.out.println("【" + table + "】 num: " + tableNum1);
+            System.out.println();
 
             List<Map<String, String>> tableDetails = MetaUtils.getPartitionTableDetails(conn, schema, partitionTable);
             Map<String, Object> partitionInfo = MetaUtils.calculatePartitionInfo(tableDetails);
-            Map<String, Integer> tableNum = MetaUtils.calculateTableNum(partitionInfo);
-            System.out.println("【" + partitionTable + "】 num: " + tableNum);
             System.out.println("【" + partitionTable + "】 partition: " + partitionInfo);
+
+            Map<String, Integer> tableNum2 = MetaUtils.calculateTableNum(partitionInfo);
+            System.out.println("【" + partitionTable + "】 num: " + tableNum2);
+            System.out.println();
+
+            List<Map<String, Object>> tableInfoList1 = new ArrayList<>();
+            Map<String, Object> tableInfo1 = new HashMap<>();
+            tableInfo1.put("tableName", tableInfo1);
+            tableInfo1.put("totalSize", tableNum2.get("totalSize"));
+            tableInfo1.put("isPartition", true);
+            tableInfo1.put("partition", partitionInfo);
+            tableInfoList1.add(tableInfo1);
+
+            List<Map<String, Object>> tableInfoList2 = new ArrayList<>();
+            Map<String, Object> tableInfo2 = new HashMap<>();
+            tableInfo2.put("tableName", partitionTable);
+            tableInfo2.put("totalSize", tableNum2.get("totalSize"));
+            tableInfo2.put("isPartition", true);
+            Map<String, Object> map1 = new HashMap<>();
+            Map<String, Integer> m1 = new HashMap<>();
+            m1.put("numRows", 15);
+            m1.put("totalSize", 468);
+            map1.put("year=2023/month=11", m1);
+            Map<String, Integer> m2 = new HashMap<>();
+            m2.put("numRows", 2);
+            m2.put("totalSize", 140);
+            map1.put("year=2022/month=12", m2);
+            map1.put("year=2022/month=11", m2);
+            tableInfo2.put("partition", map1);
+            tableInfoList2.add(tableInfo2);
+
+            List<Map<String, Object>> changeList = MetaUtils.getChangeTableAndPartition(tableInfoList1, tableInfoList2, 0);
+            System.out.println("Change table: " + changeList);
         } catch (SQLException e) {
             e.printStackTrace();
         }
