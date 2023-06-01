7c3e09ba48bda2149ac664f4f8ba5b03491ddaba
great <great__jin@163.com>
2023-06-01 14:41:59 +0800

    fix: 完善数据库连接池示例

--- demo-pool/src/main/java/xyz/ibudai/utils/LoaderUtil.java
+++ demo-pool/src/main/java/xyz/ibudai/utils/LoaderUtil.java
@@ -5,6 +5,7 @@ import xyz.ibudai.model.common.DbConst;
 import xyz.ibudai.model.common.DbType;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
@@ -53,29 +54,20 @@ public class LoaderUtil {
      * @return the class loader
      */
     public static ClassLoader getClassLoader(String driverPath) {
-        // 获取当前线程上下文加载器
-        ClassLoader parent = Thread.currentThread().getContextClassLoader();
-        URL[] urls;
+        List<URL> list = new ArrayList<>();
         try {
-            List<URL> list = new ArrayList<>();
             File driver = new File(driverPath);
             // 驱动包不存在抛出异常
             if (!driver.exists()) {
-                throw new ClassNotFoundException();
+                throw new FileNotFoundException();
             }
-            // 若为目录获取其下所有文件
-            if (driver.isDirectory()) {
-                for (File file : driver.listFiles()) {
-                    list.add(file.toURI().toURL());
-                }
-            } else {
-                // 驱动存在获取文件URL
-                list.add(driver.toURI().toURL());
-            }
-            urls = list.toArray(new URL[0]);
+            // 转为 URL 文件资源
+            list.add(driver.toURI().toURL());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return new URLClassLoader(urls, parent);
+        // 获取当前线程上下文加载器
+        ClassLoader parent = Thread.currentThread().getContextClassLoader();
+        return new URLClassLoader(list.toArray(new URL[0]), parent);
     }
 }
