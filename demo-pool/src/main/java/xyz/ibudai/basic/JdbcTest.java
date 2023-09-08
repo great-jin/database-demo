36f46dc8b3b225c467b14219eff50339b29f3d49
great <great__jin@163.com>
2023-09-08 18:06:36 +0800

    fix: 完善ResultSet示例

--- demo-pool/src/main/java/xyz/ibudai/basic/JdbcTest.java
+++ demo-pool/src/main/java/xyz/ibudai/basic/JdbcTest.java
@@ -50,7 +50,7 @@ public class JdbcTest {
                 callSt.execute();
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            throw new RuntimeException(ex);
         }
     }
 
@@ -63,13 +63,58 @@ public class JdbcTest {
         try (Connection conn = dataSource.getConnection()) {
             // Cancel auto commit
             conn.setAutoCommit(false);
-            try (Statement stmt = conn.createStatement();) {
+            try (Statement stmt = conn.createStatement()) {
                 stmt.execute(sql);
                 // Commit session
                 conn.commit();
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Use "RowSetFactory" can read data when resource is closed.
+     * <p>
+     * 1. resultSetType
+     * ==> (1) ResultSet.TYPE_FORWARD_ONLY: ResultSet only forward
+     * ==> (2) ResultSet.TYPE_SCROLL_SENSITIVE: Sensitive the change after query executed.
+     * ==> (3) ResultSet.TYPE_SCROLL_INSENSITIVE: Insensitive the change after query executed.
+     * <p>
+     * 2. resultSetConcurrency
+     * ==> (1) ResultSet.CONCUR_READ_ONLY: Default, can't update database
+     * ==> (2) ResultSet.CONCUR_UPDATABLE: Can update database
+     */
+    @Test
+    public void rollRowSetDemo() {
+        String sql = "select 'a' as name from dual";
+        try (
+                Connection conn = dataSource.getConnection();
+                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                ResultSet rs = stmt.executeQuery(sql);
+        ) {
+            // previous(): Get next row
+            while (rs.next()) {
+                // Update row content, database data not change
+                rs.updateString(1, "Alex");
+                rs.updateRow();
+            }
+
+            // previous(): Get previous row
+            while (rs.previous()) {
+            }
+
+            // relative(n): n>0: Move forward, n<0: Move backward
+            while (rs.relative(2)) {
+                System.out.println(rs.getString("name"));
+            }
+
+            // absolute(n): Set to specify row num
+            while (rs.absolute(2)) {
+                System.out.println(rs.getString("name"));
+            }
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
         }
     }
 
@@ -77,11 +122,11 @@ public class JdbcTest {
      * Use "RowSetFactory" can read data when resource is closed.
      */
     @Test
-    public void CachedRowSetDemo() {
+    public void cachedRowSetDemo() {
         String sql = "select 'a' as name from dual";
         try (
                 Connection conn = dataSource.getConnection();
-                Statement stmt = conn.createStatement();
+                Statement stmt = conn.createStatement()
         ) {
             RowSetFactory factory = RowSetProvider.newFactory();
             try (
@@ -94,9 +139,10 @@ public class JdbcTest {
                 }
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            throw new RuntimeException(ex);
         }
     }
+
 
     @After
     public void destroy() throws SQLException {
